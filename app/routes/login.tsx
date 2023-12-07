59be56034834fb396e71294f91c4eb8249dcc1d5
AchimSee <102674324+AchimSee@users.noreply.github.com>
2023-12-07 12:37:55 +0100

    login mocked

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,5 +1,43 @@
+import React, { useState } from 'react';
+
 export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    return <h1>Login Page</h1>
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    // Replace 'admin' and 'password' with your actual username and password
+    if (username === 'admin' && password === 'password') {
+      setIsLoggedIn(true);
+    } else {
+      alert('Invalid username or password');
+    }
+  };
 
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setUsername('');
+    setPassword('');
+  };
+
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <input type="submit" value="Login" />
+        </form>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
+    </div>
+  );
 }
