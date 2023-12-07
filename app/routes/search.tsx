b22c10af973705b1e4bd4fb05808039293bed7b6
AchimSee <102674324+AchimSee@users.noreply.github.com>
2023-12-07 14:49:08 +0100

    search page form

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,18 +1,56 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 
 export default function SearchPage() {
-
     const navigate = useNavigate();
-  
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState<string[]>([]);
+    const [showMore, setShowMore] = useState(false);
+
+    const handleSearch = () => {
+        // Perform search logic here using the searchTerm
+        // and update the searchResults state with the results
+        // For now, let's assume searchResults is an array of strings
+        const results = ['Result 1', 'Result 2', 'Result 3', 'Result 4', 'Result 5'];
+        setSearchResults(results);
+    };
+
+    const handleShowMore = () => {
+        setShowMore(true);
+    };
+
     const navigateToIndex = () => {
-      navigate('/');
-    }
+        navigate('/');
+    };
 
     return (
         <>
-        <h1>SearchPage</h1>
-        <button onClick={navigateToIndex}>Zurück zur Startseite</button>
+            <h1>SearchPage</h1>
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Search Results</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchResults.slice(0, showMore ? 10 : 5).map((result, index) => (
+                        <tr key={index} style={{ backgroundColor: index % 2 === 0 ? 'white' : 'gray' }}>
+                            <td>{result}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            {!showMore && searchResults.length > 5 && (
+                <button onClick={handleShowMore}>Show More</button>
+            )}
+            <button onClick={navigateToIndex}>Zurück zur Startseite</button>
         </>
     );
-
 }
