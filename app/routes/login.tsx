7f317f471f0620ed41fb7c7b696b07d6cda7ff20
DrilonCS <73757898+DrilonCS@users.noreply.github.com>
2023-12-07 13:28:02 +0100

    Navigation implementiert

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
+import { useNavigate } from '@remix-run/react';
 
 export default function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [usernameError, setUsernameError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  const navigateToSearch = () => {
+    navigate('/search');
+};
 
   const handleSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
-    // Replace 'admin' and 'password' with your actual username and password
+
     if (username === 'admin' && password === 'password') {
       setIsLoggedIn(true);
     } else {
-      alert('Invalid username or password');
+        if (username !== 'admin') {
+            setUsernameError('Invalid username');
+            setTimeout(() => setUsernameError(null), 5000);
+        }
+
+        if (password !== 'password') {
+            setPasswordError('Invalid password');
+            setTimeout(() => setPasswordError(null), 5000);
+        }
     }
   };
 
@@ -28,15 +45,20 @@ export default function LoginPage() {
           <label>
             Username:
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            {usernameError && <p>{usernameError}</p>}
           </label>
           <label>
             Password:
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {passwordError && <p>{passwordError}</p>}
           </label>
           <input type="submit" value="Login" />
         </form>
       ) : (
+        <>
         <button onClick={handleLogout}>Logout</button>
+        <button onClick={navigateToSearch}>Zum Suchformular</button>
+        </>
       )}
     </div>
   );
