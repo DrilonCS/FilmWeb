d460e52c20eac0782d232f8c3c4ff1296fa5cb46
soma1034 <mazlum-solmaz01@outlook.de>
2023-12-07 21:17:21 +0100

    Login Seite Upgrade

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
 import LoginPage from "./login";
+import log from '../log.png'; // Pfad zur Logo-Datei
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: linear-gradient(#90AFC5, #3B7EA1);
+  }
+  .center-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  .login-logo {
+    width: 400px; // Breite des Logos
+    height: auto; // Höhe des Logos automatisch anpassen
+    background: transparent; // Hintergrund des Logos transparent machen
+  }
+`;
 
 export default function Index() {
-
   return (
-  <>
-  <LoginPage />
-  </>
+    <>
+      <GlobalStyle />
+      <LoginPage />
+      <div className="center-logo">
+        <img src={log} alt="Login Logo" className="login-logo" />
+      </div>
+    </>
   );
-
 }
