1df64f5a645e202d9e02639b190b11e2e665d5c8
DrilonCS <73757898+DrilonCS@users.noreply.github.com>
2023-12-11 13:16:24 +0100

    constants.ts aktualisiert

--- app/constants.ts
+++ app/constants.ts
@@ -1,5 +1,5 @@
 export const https = 'https://'
 export const host = 'localhost:';
 export const port = '3000/';
-export const login = 'auth/login';
-export const rest = 'rest';
+export const login = 'auth/login/';
+export const rest = 'rest/';
