dacbda67d447d1c9d6e6536c67508250dcd8fca3
AchimSee <102674324+AchimSee@users.noreply.github.com>
2023-12-18 13:41:25 +0100

    Detail buttons+ config

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -4,35 +4,27 @@ import { https, host, port, rest } from '../constants';
 import { useNavigate } from 'react-router-dom';
 import { type BuchProps, Buch } from '~/Components/buch';
 
-
 function SearchPage() {
     const [id, setId] = useState('');
     const [art, setArt] = useState('');
     const [result, setResult] = useState<BuchProps | BuchProps[] | null>(null);
     const [error, setError] = useState<string | null>(null);
+    const [selectedBuch, setSelectedBuch] = useState<BuchProps | null>(null); // Added state for selected book
 
     const navigate = useNavigate();
 
     const navigateToIndex = () => {
         navigate('/');
-      };
-
+    };
 
     const handleGetByArt = () => {
-        if (!art) {
-            setError('Keine Art ausgewählt');
-            setTimeout(() => setError(null), 5000);
-            setResult(null);
-            return;
-        }
-
         axios.get(`${https}${host}${port}${rest}?art=${art}`)
             .then(response => {
                 if (response.data._embedded) {
                     setResult(response.data._embedded.buecher);
                     setError(null);
                 } else {
-                    throw new Error('Keine Bücher dieser Art vorhanden');
+                    throw new Error('Keine Buecher dieser Art vorhanden')
                 }
             })
             .catch(err => {
@@ -44,15 +36,15 @@ function SearchPage() {
 
     const handleGetId = () => {
         axios.get(`${https}${host}${port}${rest}${id}`)
-             .then(response => {
-               if (response.data._embedded) {
-                throw new Error('Type in an ID')
-               } else {
-                setResult(response.data);
-                setError(null);
-               }
+            .then(response => {
+                if (response.data._embedded) {
+                    throw new Error('Type in an ID')
+                } else {
+                    setResult(response.data);
+                    setError(null);
+                }
             })
-             .catch(err => {
+            .catch(err => {
                 setError(err.message);
                 setTimeout(() => setError(null), 5000);
                 setResult(null);
@@ -61,32 +53,40 @@ function SearchPage() {
 
     const handleGetAllBuecher = () => {
         axios.get(`${https}${host}${port}${rest}`)
-        .then(response => {
-            const buecherData = response.data._embedded.buecher.map((buch: any) => ({
-                isbn: buch.isbn,
-                rating: buch.rating,
-                art: buch.art,
-                preis: buch.preis,
-                rabatt: buch.rabatt,
-                lieferbar: buch.lieferbar,
-                datum: buch.datum,
-                homepage: buch.homepage,
-                schlagwoerter: buch.schlagwoerter,
-            }))
-            setError(null);
-            setResult(buecherData);
-        })
-        .catch(err => {
-            setError('Failed to fetch data');
-            setResult(null);
-        });
+            .then(response => {
+                const buecherData = response.data._embedded.buecher.map((buch: any) => ({
+                    isbn: buch.isbn,
+                    rating: buch.rating,
+                    art: buch.art,
+                    preis: buch.preis,
+                    rabatt: buch.rabatt,
+                    lieferbar: buch.lieferbar,
+                    datum: buch.datum,
+                    homepage: buch.homepage,
+                    schlagwoerter: buch.schlagwoerter,
+                }))
+                setError(null);
+                setResult(buecherData);
+            })
+            .catch(err => {
+                setError('Failed to fetch data');
+                setResult(null);
+            });
+    };
+
+    const handleShowDetails = (buch: BuchProps) => {
+        setSelectedBuch(buch);
+    };
+
+    const handleCloseDetails = () => {
+        setSelectedBuch(null);
     };
 
     return (
-        <div style={{ 
-            background: 'linear-gradient(#90AFC5, #3B7EA1)', 
-            minHeight: '100vh' 
-        }}>   
+        <div style={{
+            background: 'linear-gradient(#90AFC5, #3B7EA1)',
+            minHeight: '100vh'
+        }}>
             <div className="mt-3 ms-3">
                 <button onClick={navigateToIndex} className="btn btn-primary">Zurück zur Startseite</button>
             </div>
@@ -104,72 +104,94 @@ function SearchPage() {
                 <input type="text" className="form-control" value={id} onChange={(e) => setId(e.target.value)} style={{ width: '300px' }} />
             </div>
             <div style={{ marginTop: '50px' }}>
-    {result && (
-        <table className="table table-striped table-hover table-bordered">
-            <thead className="table-dark">
-                <tr>
-                    <th>ISBN</th>
-                    <th>Rating</th>
-                    <th>Art</th>
-                    <th>Preis</th>
-                    <th>Rabatt</th>
-                    <th>Lieferbar</th>
-                    <th>Datum</th>
-                    <th>Homepage</th>
-                    <th>Schlagwörter</th>
-                </tr>
-            </thead>
-            <tbody>
-            {Array.isArray(result) ? result.map((buch) => (
-                <Buch 
-                  key={buch.isbn} 
-                  isbn={buch.isbn} 
-                  rating={buch.rating} 
-                  art={buch.art} 
-                  preis={buch.preis}
-                  rabatt={buch.rabatt}
-                  lieferbar={buch.lieferbar}
-                  datum={buch.datum}
-                  homepage={buch.homepage}
-                  schlagwoerter={buch.schlagwoerter}
-                />
-            )): (
-                result ? (
-                <Buch 
-                key={result.isbn} 
-                isbn={result.isbn} 
-                rating={result.rating} 
-                art={result.art} 
-                preis={result.preis}
-                rabatt={result.rabatt}
-                lieferbar={result.lieferbar}
-                datum={result.datum}
-                homepage={result.homepage}
-                schlagwoerter={result.schlagwoerter}
-            />
-                ) : null
-        )}
-            </tbody>
-        </table>
-    )}
-    {error && <p>{error}</p>}
-    </div>
-    </div>
+                {result && (
+                    <table className="table table-striped table-hover table-bordered">
+                        <thead className="table-dark">
+                            <tr>
+                                <th>ISBN</th>
+                                <th>Rating</th>
+                                <th>Art</th>
+                                <th>Preis</th>
+                                <th>Rabatt</th>
+                                <th>Lieferbar</th>
+                                <th>Datum</th>
+                                <th>Homepage</th>
+                                <th>Schlagwörter</th>
+                                <th>Details</th> {/* Added table header for details */}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Array.isArray(result) ? result.map((buch) => (
+                                <tr key={buch.isbn}>
+                                    <td>{buch.isbn}</td>
+                                    <td>{buch.rating}</td>
+                                    <td>{buch.art}</td>
+                                    <td>{buch.preis}</td>
+                                    <td>{buch.rabatt}</td>
+                                    <td>{buch.lieferbar}</td>
+                                    <td>{buch.datum}</td>
+                                    <td>{buch.homepage}</td>
+                                    <td>{buch.schlagwoerter}</td>
+                                    <td>
+                                        <button onClick={() => handleShowDetails(buch)} className="btn btn-primary">Details</button> {/* Added button for details */}
+                                    </td>
+                                </tr>
+                            )) : (
+                                result ? (
+                                    <tr key={result.isbn}>
+                                        <td>{result.isbn}</td>
+                                        <td>{result.rating}</td>
+                                        <td>{result.art}</td>
+                                        <td>{result.preis}</td>
+                                        <td>{result.rabatt}</td>
+                                        <td>{result.lieferbar}</td>
+                                        <td>{result.datum}</td>
+                                        <td>{result.homepage}</td>
+                                        <td>{result.schlagwoerter}</td>
+                                        <td>
+                                            <button onClick={() => handleShowDetails(result)} className="btn btn-primary">Details</button> {/* Added button for details */}
+                                        </td>
+                                    </tr>
+                                ) : null
+                            )}
+                        </tbody>
+                    </table>
+                )}
+                {error && <p>{error}</p>}
+            </div>
+            {selectedBuch && ( // Added condition to show details popup
+                <div className="popup">
+                    <div className="popup-content">
+                        <button onClick={handleCloseDetails} className="close-btn">X</button>
+                        <h2>Details</h2>
+                        <p>ISBN: {selectedBuch.isbn}</p>
+                        <p>Rating: {selectedBuch.rating}</p>
+                        <p>Art: {selectedBuch.art}</p>
+                        <p>Preis: {selectedBuch.preis}</p>
+                        <p>Rabatt: {selectedBuch.rabatt}</p>
+                        <p>Lieferbar: {selectedBuch.lieferbar}</p>
+                        <p>Datum: {selectedBuch.datum}</p>
+                        <p>Homepage: {selectedBuch.homepage}</p>
+                        <p>Schlagwörter: {selectedBuch.schlagwoerter}</p>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
 function withAuth(Component: React.ComponentType) {
     return function ProtectedRoute(props: any) {
-      const navigate = useNavigate();
-      const token = localStorage.getItem('authToken');
-  
-      if (!token) {
-        navigate('/');
-        return null;
-      }
-      return <Component {...props} />;
+        const navigate = useNavigate();
+        const token = localStorage.getItem('authToken');
+
+        if (!token) {
+            navigate('/');
+            return null;
+        }
+        return <Component {...props} />;
     };
-  }
-  
-  const ProtectedSearchPage = withAuth(SearchPage);
-  export default ProtectedSearchPage;
+}
+
+const ProtectedSearchPage = withAuth(SearchPage);
+export default ProtectedSearchPage;
