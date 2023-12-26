201b6e46b6914788621611ba105b0f68d3665a62
tekoshin63 <t.demir128@gmail.com>
2023-12-26 16:14:52 +0100

    gmail logo

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import gmailLogo from './gmail.png';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="d-flex justify-content-center mt-5">
+    <footer className='d-flex justify-content-center mt-5'>
       <div>
-        <p> <h2>Authors :</h2>
-          <br />
-          <a href="mailto:j.drilon99@gmail.com" style={{ color: 'darkred' }}><h3> Drilon,</h3></a>
-          <br />
-          <a href="mailto:j.drilon99@gmail.com" style={{ color: 'darkred' }}><h3> Tekin,</h3></a>
-          <br />
-          <a href="mailto:j.drilon99@gmail.com" style={{ color: 'darkred' }}><h3> Achim,</h3></a>
-          <br />
-          <a href="mailto:j.drilon99@gmail.com" style={{ color: 'darkred' }}><h3> Mazlum</h3></a>
+        <p>
+          <h2>Authors :</h2>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <a href='mailto:j.drilon99@gmail.com' style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={gmailLogo} alt="Email Logo" style={{ height: '20px', width: '20px' }} />
+              <h3 style={{ color: 'darkred' }}> Drilon,</h3>
+            </a>
+            <a href='mailto:j.drilon99@gmail.com' style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={gmailLogo} alt="Email Logo" style={{ height: '20px', width: '20px' }} />
+              <h3 style={{ color: 'darkred' }}> Tekin,</h3>
+            </a>
+            <a href='mailto:j.drilon99@gmail.com' style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={gmailLogo} alt="Email Logo" style={{ height: '20px', width: '20px' }} />
+              <h3 style={{ color: 'darkred' }}> Achim,</h3>
+            </a>
+            <a href='mailto:j.drilon99@gmail.com' style={{ display: 'flex', alignItems: 'center' }}>
+              <img src={gmailLogo} alt="Email Logo" style={{ height: '20px', width: '20px' }} />
+              <h3 style={{ color: 'darkred' }}> Mazlum</h3>
+            </a>
+          </div>
         </p>
         <p><h5>SWE Projekt bei Prof. Jürgen Zimmermann</h5></p>
       </div>
