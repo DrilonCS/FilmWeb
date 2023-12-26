a7ef93b9a03b14193e4ecb94f56a8f2fa2f11389
tekoshin63 <t.demir128@gmail.com>
2023-12-26 18:41:27 +0100

    hover effekte auf login

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -3,59 +3,71 @@ import gmailLogo from './gmail.png';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="d-flex justify-content-center mt-5">
+    <footer className="d-flex justify-content-center mt-5  box">
       <div>
+        <h2>Authors :</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <h3 style={{ color: 'darkred' }}> Drilon</h3>
+              </td>
+              <td>
+                <a href="mailto:j.drilon99@gmail.com">
+                  <img
+                    src={gmailLogo}
+                    alt="Email Logo"
+                    style={{ height: '20px', width: '20px' }}
+                  />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3 style={{ color: 'darkred' }}> Tekin</h3>
+              </td>
+              <td>
+                <a href="mailto:t.demir128@gmail.com" >
+                  <img
+                    src={gmailLogo}
+                    alt="Email Logo"
+                    style={{ height: '20px', width: '20px' }}
+                  />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3 style={{ color: 'darkred' }}> Mazlum</h3>
+              </td>
+              <td>
+                <a href="mailto:j.drilon99@gmail.com" >
+                  <img
+                    src={gmailLogo}
+                    alt="Email Logo"
+                    style={{ height: '20px', width: '20px' }}
+                  />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3 style={{ color: 'darkred' }}> Achim</h3>
+              </td>
+              <td>
+                <a href="mailto:j.drilon99@gmail.com">
+                  <img
+                    src={gmailLogo}
+                    alt="Email Logo"
+                    style={{ height: '20px', width: '20px' }}
+                  />
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <p>
-          <h2>Authors :</h2>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <a
-              href="mailto:j.drilon99@gmail.com"
-              style={{ display: 'flex', alignItems: 'center' }}
-            >
-              <img
-                src={gmailLogo}
-                alt="Email Logo"
-                style={{ height: '20px', width: '20px' }}
-              />
-              <h3 style={{ color: 'darkred' }}> Drilon,</h3>
-            </a>
-            <a
-              href="mailto:j.drilon99@gmail.com"
-              style={{ display: 'flex', alignItems: 'center' }}
-            >
-              <img
-                src={gmailLogo}
-                alt="Email Logo"
-                style={{ height: '20px', width: '20px' }}
-              />
-              <h3 style={{ color: 'darkred' }}> Tekin,</h3>
-            </a>
-            <a
-              href="mailto:j.drilon99@gmail.com"
-              style={{ display: 'flex', alignItems: 'center' }}
-            >
-              <img
-                src={gmailLogo}
-                alt="Email Logo"
-                style={{ height: '20px', width: '20px' }}
-              />
-              <h3 style={{ color: 'darkred' }}> Achim,</h3>
-            </a>
-            <a
-              href="mailto:j.drilon99@gmail.com"
-              style={{ display: 'flex', alignItems: 'center' }}
-            >
-              <img
-                src={gmailLogo}
-                alt="Email Logo"
-                style={{ height: '20px', width: '20px' }}
-              />
-              <h3 style={{ color: 'darkred' }}> Mazlum</h3>
-            </a>
-          </div>
-        </p>
-        <p>
-          <h5>SWE Projekt bei Prof. Jürgen Zimmermann</h5>
+          <h5>© 2023 SWE Projekt bei Prof. Jürgen Zimmermann</h5>
         </p>
       </div>
     </footer>
