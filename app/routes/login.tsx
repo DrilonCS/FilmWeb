658c2d6086468e1ba85cfc3f287b190d7a94cc30
soma1034 <mazlum-solmaz01@outlook.de>
2024-01-01 12:36:23 +0100

    Komponenten ausgelagert

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { https, host, login, port } from '../constants';
 import logo from '~/log.png';
 import { LoginForm } from '../components/Login/LoginFormComponent';
-import { UserActions } from '../components/LoginActionsComponent';
+import { UserActions } from '../components/Login/LoginActionsComponent';
 import { Footer } from '../components/FooterComponent';
 
 export function LoginPage() {
