a2f29a559a521b52befa4fdc1db0d56d1885c69a
DrilonCS <73757898+DrilonCS@users.noreply.github.com>
2024-01-03 21:30:59 +0100

    Revert "Footer"
    This reverts commit 2be1620fb93366d13bce8aba15c4e081be1e18ad.

--- app/components/FooterComponent.tsx
+++ app/components/FooterComponent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import gmailLogo from './gmail.png';
-import { Container, Row, Col } from 'react-bootstrap';
 
 export const Footer: React.FC = () => {
   return (
@@ -14,27 +13,26 @@ export const Footer: React.FC = () => {
         width: '100%',
       }}
     >
-      <Container
+      <div
         className="navbar"
         style={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           width: '100%',
-          maxWidth: '100%', // Set the width to 100% to match the page width
           backgroundColor: 'white',
           borderRadius: '5px',
           padding: '10px',
         }}
       >
-        <Row
+        <div
           style={{
             display: 'flex',
             justifyContent: 'space-between',
-            width: '100%', // Adjusted width to take up the whole page
+            width: '25%',
           }}
         >
-          <Col style={{ display: 'flex', alignItems: 'center' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <h3 style={{ color: 'black', marginRight: '10px' }}> Drilon</h3>
             <a
               href="mailto:j.drilon99@gmail.com"
@@ -46,8 +44,8 @@ export const Footer: React.FC = () => {
                 style={{ height: '28px', width: '25px' }}
               />
             </a>
-          </Col>
-          <Col style={{ display: 'flex', alignItems: 'center' }}>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <h3 style={{ color: 'black', marginRight: '10px' }}> Tekin</h3>
             <a
               href="mailto:t.demir128@gmail.com"
@@ -59,8 +57,8 @@ export const Footer: React.FC = () => {
                 style={{ height: '28px', width: '25px' }}
               />
             </a>
-          </Col>
-          <Col style={{ display: 'flex', alignItems: 'center' }}>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <h3 style={{ color: 'black', marginRight: '10px' }}> Mazlum</h3>
             <a href="mailto:mazlum1034@h-k.de" style={{ marginRight: '15px' }}>
               <img
@@ -69,8 +67,8 @@ export const Footer: React.FC = () => {
                 style={{ height: '28px', width: '25px' }}
               />
             </a>
-          </Col>
-          <Col style={{ display: 'flex', alignItems: 'center' }}>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
             <h3 style={{ color: 'black', marginRight: '10px' }}> Achim</h3>
             <a href="mailto:achim@seelhorst.net">
               <img
@@ -79,9 +77,9 @@ export const Footer: React.FC = () => {
                 style={{ height: '28px', width: '25px' }}
               />
             </a>
-          </Col>
-        </Row>
-      </Container>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
