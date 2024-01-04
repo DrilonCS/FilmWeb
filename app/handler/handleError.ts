ef167dccb0ff3ef30f4bf3d2c466027805e24700
tekoshin63 <t.demir128@gmail.com>
2024-01-04 21:00:36 +0100

    eslint konfig

--- app/handler/handleError.ts
+++ app/handler/handleError.ts
@@ -75,8 +75,10 @@ export const handleCreateError = (
             });
         });
         setErrors(newErrors);
+        setTimeout(() => setErrors({}), 5000);
     } else {
         setError(error.message);
+        setTimeout(() => setError(null), 5000);
     }
 };
 
