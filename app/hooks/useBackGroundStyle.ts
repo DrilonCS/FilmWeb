98e2de6d1216861f6144070b3807b1fea4b5bd1d
tekoshin63 <t.demir128@gmail.com>
2024-01-04 19:33:24 +0100

    prettier neu

--- app/hooks/useBackGroundStyle.ts
+++ app/hooks/useBackGroundStyle.ts
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 
 export const useBackgroundStyle = () => {
-  useEffect(() => {
-    const originalStyleHtml = window.getComputedStyle(
-      document.documentElement,
-    ).background;
-    const originalStyleBody = window.getComputedStyle(document.body).background;
-    document.documentElement.style.background =
-      'linear-gradient(#90AFC5, #3B7EA1)';
-    document.body.style.background = 'linear-gradient(#90AFC5, #3B7EA1)';
-    document.body.style.margin = '0';
-    document.body.style.padding = '0';
-    return () => {
-      document.documentElement.style.background = originalStyleHtml;
-      document.body.style.background = originalStyleBody;
-      document.body.style.margin = '';
-      document.body.style.padding = '';
-    };
-  }, []);
+    useEffect(() => {
+        const originalStyleHtml = window.getComputedStyle(
+            document.documentElement,
+        ).background;
+        const originalStyleBody = window.getComputedStyle(
+            document.body,
+        ).background;
+        document.documentElement.style.background =
+            'linear-gradient(#90AFC5, #3B7EA1)';
+        document.body.style.background = 'linear-gradient(#90AFC5, #3B7EA1)';
+        document.body.style.margin = '0';
+        document.body.style.padding = '0';
+        return () => {
+            document.documentElement.style.background = originalStyleHtml;
+            document.body.style.background = originalStyleBody;
+            document.body.style.margin = '';
+            document.body.style.padding = '';
+        };
+    }, []);
 };
